8f9da6aeedad1e8be0898f082ee4565d816b8bfc
Alon Swartz <alon@turnkeylinux.org>
2012-12-05 13:29:36 +0200

    updated copyright/license

--- pylib/cmd_deckdebuild.py
+++ pylib/cmd_deckdebuild.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
+# Copyright (c) TurnKey Linux - http://www.turnkeylinux.org
+#
+# This file is part of DeckDebuild
+#
+# DeckDebuild is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+
 """Syntax: $AV0 [-options] /path/to/buildroot [ /path/to/output-dir ]
 build a Debian package in a decked chroot
 
